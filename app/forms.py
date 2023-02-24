7a84c263c0c9994dab77afc66dc4a0cf2e3d29ee
Your Name <you@example.com>
2023-02-24 09:11:56 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from app.models import *
+
+class StudentForm(forms.Form):
+    name=forms.CharField(max_length=100)
+    age=forms.IntegerField()
+
+class GFListForm(forms.ModelForm):
+    class Meta:
+        model=GFList
+        fields='__all__'
